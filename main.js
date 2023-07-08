e3e608c62fe507210c8212c44673b5557efaddf7
prakhar20394 <88589942+prakhar20394@users.noreply.github.com>
2023-07-08 20:15:48 +0530

    Entire website change

--- main.js
+++
@@ -1,6 +0,0 @@
-const helloButton = document.getElementById('.hamburger-menu');
-const menu = document.getElementById('.menu');
-
-helloButton.addEventListener('click', function() {
-  menu.classList.toggle('hidden');
-});
