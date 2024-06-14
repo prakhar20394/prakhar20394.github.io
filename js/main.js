300b2efd2637089748258ab25ccc0ecd3912819e
Deep Sharma <deepsharma.cs@gmail.com>
2024-06-14 16:30:17 +0530

    (Experimental) Remove spinner animation

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(window).on('load', function() {
 		Preloder
 	--------------------*/
 	$(".loader").fadeOut();
-	$("#preloder").delay(400).fadeOut("slow");
+	$("#preloder").delay(400).fadeOut("fast");
 
 });
 
