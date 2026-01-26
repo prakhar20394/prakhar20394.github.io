03cc13a0c4c23f531d4f094eada5b483f737106c
Prakhar RipikAI <prakhar@ripik.ai>
2026-01-26 20:50:34 +0530

    package changes.

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
-
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-dom/client"],
-  },
-
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
-  },
+  resolve: { dedupe: ["react", "react-dom"] },
 });
